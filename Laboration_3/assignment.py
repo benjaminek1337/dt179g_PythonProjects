d69a2f3977af473ad0be393db76581ae5d9b2c37
Benjamin Ek <benjaminek@live.se>
2020-12-01 10:02:01 +0100

    everything done except fib_memory function

--- Laboration_3/assignment.py
+++ Laboration_3/assignment.py
@@ -30,15 +30,29 @@ LOGGER = None  # declared at module level, will be defined from main()
 
 def create_logger() -> logging.Logger:
     """Create and return logger object."""
-    pass  # TODO: Replace with implementation!
+    file_path = RESOURCES / "ass3_log_conf.json"  # Get the filepath to the logger config file
+    with open(file_path, "r") as file:  # Open the logger config file
+        config = json.load(file)  # Write the json data to variable
+        logging.config.dictConfig(config)  # Configure logger
+        return logging.getLogger("ass_3_logger")  # Return logger with specified name
 
 
 def measurements_decorator(func):
     """Function decorator, used for time measurements."""
     @wraps(func)
     def wrapper(nth_nmb: int) -> tuple:
-        pass  # TODO: Replace with implementation!
-
+        fibonacci_values = list()  # List of fibonacci values, to be filled
+        start_time = timer()  # Using imported timer to get current time in ms
+        LOGGER.info("Starting measurements...")  # Initialize logging with starting measurements statement
+        for i in range(nth_nmb, -1, -1):  # Increment fibonacci-sequence in reverse, from starting value to 0
+            fib_val = func(i)  # Run the fib functions to get the current value in sequence
+            fibonacci_values.append(fib_val)  # Write value to list
+            if i % 5 == 0:  # For every 5 numbers
+                LOGGER.debug(f"{i}: {fib_val}")  # Log the sequence, and value of sequence
+        end_time = timer()  # Get current time, after the loop is finished
+        duration = end_time - start_time  # Get the elapsed time between start and finish
+        output = (duration, fibonacci_values)  # Write the elapsed time and list of values to a tuple
+        return output  # Return the tuple
     return wrapper
 
 
@@ -63,10 +77,17 @@ def fibonacci_recursive(nth_nmb: int) -> int:
     return fib(nth_nmb)
 
 
+memory = {0: 0, 1: 1}
+
+
 @measurements_decorator
 def fibonacci_memory(nth_nmb: int) -> int:
     """An recursive approach to find Fibonacci sequence value, storing those already calculated."""
-    pass  # TODO: Replace with implementation!
+    def fib(_n):
+        return _n if _n <= 1 else fib(_n - 1) + fib(_n - 2)
+    if nth_nmb not in memory:
+        memory[nth_nmb] = fib(nth_nmb)
+    return memory[nth_nmb]
 
 
 def duration_format(duration: float, precision: str) -> str:
@@ -86,12 +107,34 @@ def duration_format(duration: float, precision: str) -> str:
 
 def print_statistics(fib_details: dict, nth_value: int):
     """Function which handles printing to console."""
-    pass  # TODO: Replace with implementation!
+    print("{0}\n{1}{0}".format(LINE, f"DURATION FOR EACH APPROACH WITHIN INTERVAL: {nth_value}-0".center(len(LINE))))
+
+    table_data = [  # Initialize list of table data to be printed with column headers
+        ["", "Seconds", "Milliseconds", "Microseconds", "Nanoseconds"]
+    ]
+
+    for key in fib_details:  # Iterate the dictionary
+        table_data.append([
+            key.title(),  # Get the current key
+            duration_format(fib_details[key][0], "Seconds"),  # Pass the ms value linked to the key to be formatted
+            duration_format(fib_details[key][0], "Milliseconds"),
+            duration_format(fib_details[key][0], "Microseconds"),
+            duration_format(fib_details[key][0], "Nanoseconds"),
+        ])
+    for i in range(len(table_data)):  # Iterate the table_data list, format "table", print values
+        print("{: <13} {: >13} {: >15} {: >15} {: >15}".format(*table_data[i]))
 
 
 def write_to_file(fib_details: dict):
     """Function to write information to file."""
-    pass  # TODO: Replace with implementation!
+    for key in fib_details:  # Iterate through dict
+        file_name = key.replace(" ", "_") + ".txt"
+        with open(RESOURCES / file_name, "w") as file1:  # Open/create file with key as basis
+            # for filename
+            fib_details[key][1].reverse()  # Reverse the logged fibonacci values in dict
+            for i in range(len(fib_details[key][1]) - 1, -1, -1):  # Iterate the fibonacci values in dict
+                value = fib_details[key][1][i]  # Write the current value to variable
+                file1.write(f"{i}: {value}\n")  # Write the value + sequence nr to file
 
 
 def main():
